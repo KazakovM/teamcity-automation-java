bcfadaaf00dcf3283bc9181438b16ccf2a487349
Mikhail Kazakov <kazakovms93@gmail.com>
2025-02-13 23:10:21 +0300

    Add CheckedBase and UncheckedBase classes

--- src/main/java/org/example/teamcity/api/enums/Endpoint.java
+++ src/main/java/org/example/teamcity/api/enums/Endpoint.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.example.teamcity.api.models.BaseModel;
 import org.example.teamcity.api.models.BuildType;
+import org.example.teamcity.api.models.Project;
 
 @AllArgsConstructor
 @Getter
 public enum Endpoint {
-    BUILD_TYPES("/app/rest/buildTypes", BuildType.class);
+    BUILD_TYPES("/app/rest/buildTypes", BuildType.class),
+    PROJECT("/app/rest/projects", Project.class);
 
     private final String url;
     private final Class<? extends BaseModel> modelClass;
