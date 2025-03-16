e04aa2b7531f40c58ac8c481c26fafb75d9eb99a
mikhail kazakov <kazakovms93@gmail.com>
2025-03-16 20:20:50 +0300

    Add UI test for build type creation

---
+++ src/main/java/org/example/teamcity/ui/constants/ErrorMessages.java
@@ -0,0 +1,5 @@
+package org.example.teamcity.ui.constants;
+
+public class ErrorMessages {
+    public static final String ERROR_BUILD_TYPE_EMPTY_NAME = "Build configuration name must not be empty";
+}
--- src/main/java/org/example/teamcity/ui/pages/ProjectPage.java
+++ src/main/java/org/example/teamcity/ui/pages/ProjectPage.java
@@ -1,17 +1,33 @@
 package org.example.teamcity.ui.pages;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import org.example.teamcity.ui.elements.BuildTypeElement;
+
+import java.util.List;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class ProjectPage extends BasePage {
     private static final String PROJECT_URL = "/project/%s";
 
     public SelenideElement title = $("span[class*='ProjectPageHeader']");
 
+    private ElementsCollection buildTypeElements = $$(".BuildTypes__item--UX");
+
+    public ProjectPage() {
+        title.shouldBe(Condition.visible, BASE_WAITING);
+    }
+
     public static ProjectPage open(String projectId) {
         return Selenide.open(PROJECT_URL.formatted(projectId), ProjectPage.class);
     }
+
+    public List<BuildTypeElement> getBuildTypes() {
+        return generatePageElements(buildTypeElements, BuildTypeElement::new);
+    }
+
 }
