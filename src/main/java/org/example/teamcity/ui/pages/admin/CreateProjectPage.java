19c5c4409da4867fa04289936a6b396ca07b7c48
mikhail kazakov <kazakovms93@gmail.com>
2025-04-01 00:57:57 +0300

    Try to fix ui tests

--- src/main/java/org/example/teamcity/ui/pages/admin/CreateProjectPage.java
+++ src/main/java/org/example/teamcity/ui/pages/admin/CreateProjectPage.java
@@ -2,6 +2,7 @@ package org.example.teamcity.ui.pages.admin;
 
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 import org.example.teamcity.ui.pages.ProjectsPage;
 
 import static com.codeborne.selenide.Selenide.$;
@@ -12,6 +13,7 @@ public class CreateProjectPage extends CreateBasePage {
 
     private SelenideElement projectNameInput = $("#projectName");
 
+    @Step("Open Project Page")
     public static CreateProjectPage open(String projectId) {
         return Selenide.open(CREATE_URL.formatted(projectId, PROJECT_SHOW_MODE), CreateProjectPage.class);
     }
